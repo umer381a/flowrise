8ff65b6051a9fb2af56f6453dbc762ce393afc38
umer farooq <urfq_farooq@hotmail.com>
2023-11-23 21:49:24 +0500

    feat: Add homepage page type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,23 @@
+import { Metadata } from "next";
 
-export default function Home() {
-  return (
-    <main className="">
-      <h1 className="font-display text-5xl font-bold">Hello world!</h1>
-    </main>
-  )
+import { createClient } from "@/prismicio";
+// import { components } from "@/slices";
+
+export default async function Page() {
+  const client = createClient();
+  const page = await client.getSingle("homepage");
+
+  return <div>it works</div>
+
+  // return <SliceZone slices={page.data.slices} components={components} />;
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+  const client = createClient();
+  const page = await client.getSingle("homepage");
+
+  return {
+    title: page.data.meta_title,
+    description: page.data.meta_description,
+  };
 }
